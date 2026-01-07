d8d6593f75836bc25f17a6cbdfadc09c3224612e
Ohk Soe Htet <ohksoe2002@gmail.com>
2026-01-07 18:30:08 +0800

    add tw and tw-shadcn

---
+++ ts/tw/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+	return (
+		<>
+			<h1 className="text-3xl font-bold underline">Hello World</h1>
+			<p>Work Smart && Hard, !(Hard || Smart)</p>
+		</>
+	);
+}
+
+export default App;
