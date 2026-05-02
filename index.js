2a031b0a1e2c2bd3eb3f9aa34c9e5a4cf56b947d
Ohk Soe Htet <ohksoe2002@gmail.com>
2026-05-02 16:23:05 +0800

    chore: update version to 2.0.0 in package.json

--- index.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-
-import chalk from "chalk";
-import { run } from "./app/cli.js";
-
-run().catch((error) => {
-	console.error(chalk.red("An error occurred:"), error);
-	process.exit(1);
-});
